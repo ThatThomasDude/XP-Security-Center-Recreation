6440f20b3510ce9267f0e02618bd443d8d00c3c1
hjdfhdsgdr <157922892+hjdfhdsgdr@users.noreply.github.com>
2025-06-11 12:42:11 +0700

    Initial commit

---
+++ src/XPSecurityCenter/Properties/Resources.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace XPSecurityCenter.Properties
+{
+    internal class Resources
+    {
+        internal static Image resourcespanel;
+        internal static Image _31;
+        internal static Image _1;
+        internal static Image _2;
+        internal static Image inetcpl;
+    }
+}
